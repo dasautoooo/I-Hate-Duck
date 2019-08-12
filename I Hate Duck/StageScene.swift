68097fc5af28a8762bc0ba8c729adf4013e03c2e
Leonard Chan <wxclx98@gmail.com>
2019-08-12 19:53:10 +0800

    Add Target class.  Let scene automatically pick number to animate ducks.

--- I Hate Duck/StageScene.swift
+++ I Hate Duck/StageScene.swift
@@ -9,15 +9,27 @@
 import SpriteKit
 import GameplayKit
 
-class Target: SKNode { }
-
 class StageScene: SKScene {
+    
+    var duckMoveDuration: TimeInterval!
 
     override func didMove(to view: SKView) {
-        let duck = generateDuck(hasTarget: true)
-        duck.position = CGPoint(x: 200, y: 80)
-        duck.zPosition = 6
-        addChild(duck)
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            let duck = self.generateDuck(hasTarget: Bool.random())
+            duck.position = CGPoint(x: -10, y: Int.random(in: 60...90))
+            duck.zPosition = Int.random(in: 0...1) == 0 ? 4 : 6
+            self.addChild(duck)
+            
+            if duck.hasTarget {
+                self.duckMoveDuration = TimeInterval(Int.random(in: 2...4))
+            } else {
+                self.duckMoveDuration = TimeInterval(Int.random(in: 5...7))
+            }
+            
+            duck.run(.sequence([
+                .moveTo(x: 850, duration: self.duckMoveDuration),
+                .removeFromParent()]))
+        }
     }
 
 }
