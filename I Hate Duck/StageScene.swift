49ea4d788b3199e857bffc096797495b4477218c
Leonard Chan <wxclx98@gmail.com>
2019-08-12 21:48:28 +0800

    Add functions to generate targets and active targets.

--- I Hate Duck/StageScene.swift
+++ I Hate Duck/StageScene.swift
@@ -12,24 +12,12 @@ import GameplayKit
 class StageScene: SKScene {
     
     var duckMoveDuration: TimeInterval!
+    let targetXPosition: [Int] = [160, 240, 320, 400, 480, 560, 640]
+    var usingTargetXPosition = Array<Int>()
 
     override func didMove(to view: SKView) {
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
-            let duck = self.generateDuck(hasTarget: Bool.random())
-            duck.position = CGPoint(x: -10, y: Int.random(in: 60...90))
-            duck.zPosition = Int.random(in: 0...1) == 0 ? 4 : 6
-            self.addChild(duck)
-            
-            if duck.hasTarget {
-                self.duckMoveDuration = TimeInterval(Int.random(in: 2...4))
-            } else {
-                self.duckMoveDuration = TimeInterval(Int.random(in: 5...7))
-            }
-            
-            duck.run(.sequence([
-                .moveTo(x: 850, duration: self.duckMoveDuration),
-                .removeFromParent()]))
-        }
+        activeDucks()
+        activeTargets()
     }
 
 }
@@ -53,6 +41,7 @@ extension StageScene {
         duck = SKSpriteNode(imageNamed: duckImageName)
         stick = SKSpriteNode(imageNamed: "stick/\(Int.random(in: 1...2))")
         stick.anchorPoint = CGPoint(x: 0.5, y: 0)
+        stick.position = CGPoint(x: 0, y: 0)
         duck.position = CGPoint(x: 0, y: 170)
         node.xScale = 0.8
         node.yScale = 0.8
@@ -61,6 +50,74 @@ extension StageScene {
         
         
         return node
+    }
+    
+    func generateTarget() -> Target {
+        var target: SKSpriteNode
+        var stick: SKSpriteNode
+        let node = Target()
+        
+        target = SKSpriteNode(imageNamed: "target/\(Int.random(in: 1...3))")
+        target.xScale = 0.5
+        target.yScale = 0.5
+        target.position = CGPoint(x: 0, y: 95)
+        
+        stick = SKSpriteNode(imageNamed: "stick_metal")
+        stick.xScale = 0.5
+        stick.yScale = 0.5
+        stick.anchorPoint = CGPoint(x: 0.5, y: 0)
+        stick.position = CGPoint(x: 0, y: 0)
+        
+        node.addChild(stick)
+        node.addChild(target)
+        
+        return node
+    }
+    
+    func activeDucks() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            let duck = self.generateDuck(hasTarget: Bool.random())
+            duck.position = CGPoint(x: -10, y: Int.random(in: 60...90))
+            duck.zPosition = Int.random(in: 0...1) == 0 ? 4 : 6
+            self.addChild(duck)
+            
+            if duck.hasTarget {
+                self.duckMoveDuration = TimeInterval(Int.random(in: 2...4))
+            } else {
+                self.duckMoveDuration = TimeInterval(Int.random(in: 5...7))
+            }
+            
+            duck.run(.sequence([
+                .moveTo(x: 850, duration: self.duckMoveDuration),
+                .removeFromParent()]))
+        }
+    }
+    
+    func activeTargets() {
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
+            let target = self.generateTarget()
+            var xPosition = self.targetXPosition.randomElement()!
+            
+            while self.usingTargetXPosition.contains(xPosition) {
+                xPosition = self.targetXPosition.randomElement()!
+            }
+            
+            self.usingTargetXPosition.append(xPosition)
+            target.position = CGPoint(x: xPosition, y: Int.random(in: 120...145))
+            target.zPosition = 1
+            target.yScale = 0
+            self.addChild(target)
+            
+            target.run(.sequence([
+                .scaleY(to: 1, duration: 0.2),
+                .wait(forDuration: TimeInterval(Int.random(in: 3...6))),
+                .scaleY(to: 0, duration: 0.2),
+                .removeFromParent(),
+                .run {
+                    self.usingTargetXPosition.remove(at: self.usingTargetXPosition.firstIndex(of: xPosition)!)
+                }]))
+            
+        }
     }
 }
 
