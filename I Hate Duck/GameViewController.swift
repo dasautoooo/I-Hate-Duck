fd9e7baae029414a31d929dc3d8cb6594afe650d
Leonard Chan <wxclx98@gmail.com>
2019-08-12 17:25:46 +0800

    Initial Commit

---
+++ I Hate Duck/GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  I Hate Duck
+//
+//  Created by Leonard Chen on 8/12/19.
+//  Copyright © 2019 Leonard Chan. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+
+            if let scene = SKScene(fileNamed: "StageScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = false
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ I Hate Duck/StageScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  I Hate Duck
+//
+//  Created by Leonard Chen on 8/12/19.
+//  Copyright © 2019 Leonard Chan. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class StageScene: SKScene {
+
+    override func didMove(to view: SKView) {
+
+    }
+
+}
